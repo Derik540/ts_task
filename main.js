f20389370f424c88957a7358cdb749b2ee3ecb63
Derik Nascimento <79671639+Derik540@users.noreply.github.com>
2024-01-11 10:26:32 -0300

    feat/task ts

---
+++ main.js
@@ -0,0 +1,9 @@
+function multiplica(num1, num2) {
+    return num1 * num2;
+}
+var resultado = multiplica(23, 45);
+console.log(resultado);
+function saudacao(nome) {
+    console.log("Bom dia ".concat(nome));
+}
+saudacao('Derik');
---
+++ main.ts
@@ -0,0 +1,13 @@
+function multiplica(num1: number, num2: number) {
+    return num1 * num2;
+}
+
+let resultado = multiplica(23, 45);
+console.log(resultado);
+
+function saudacao(nome: string) {
+    console.log(`Bom dia ${nome}`);
+}
+
+saudacao('Derik');
+
